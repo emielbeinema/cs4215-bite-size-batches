33a32906b72d3a59bb1cfc4f5041bf71483b36ca
Emiel Beinema <emiel.beinema@gmail.com>
2019-10-16 22:20:03 +0200

    Remove num_nodes 3, reinstate batch_size 32

--- experiments/exploration/fractional-generator.py
+++ experiments/exploration/fractional-generator.py
@@ -6,16 +6,17 @@ import pandas as pd
 # num nodes: 1, 2, 3, 4
 # memory size: 1024, 1536, 2048
 
-levels = [5, 4, 3]
+levels = [6, 3, 3]
 reduction = 2
 
 df = pd.DataFrame(pyDOE2.gsd(levels, reduction), columns=[
     'batch_size', 'num_nodes', 'memory_size'
 ])
-df['batch_size'] = df['batch_size'].map({0: 64, 1: 128, 2: 256, 3: 512, 4: 1024})
-df['num_nodes'] = df['num_nodes'].map({0: 1, 1: 2, 2: 3, 3: 4})
+df['batch_size'] = df['batch_size'].map({0: 32, 1: 64, 2: 128, 3: 256, 4: 512, 5: 1024})
+df['num_nodes'] = df['num_nodes'].map({0: 1, 1: 2, 2: 4})
 df['memory_size'] = df['memory_size'].map({0: 1024, 1: 1536, 2: 2048})
 
 design_with_repeats = pd.DataFrame(np.repeat(df.values, 5, axis=0))
 
-design_with_repeats.to_csv("gsd2.csv")
+design_with_repeats.to_csv("gsd3.csv")
+
